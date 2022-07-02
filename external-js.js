e241b7f5dea8cfd110dc6f6ed904e354003b7f4d
Red <crabmarket56@gmail.com>
2022-07-02 12:25:18 -0400

    Add functionality to delete cells upon reset button and populate grid with inputted squares

--- external-js.js
+++ external-js.js
@@ -7,9 +7,6 @@ const content = document.querySelector(".content");
 const button = document.createElement("button");
 content.appendChild(button);
 
-let width = container.clientWidth;
-console.log(width);
-
 
 
 for (i = 0; i <= 256; i++) {
@@ -19,16 +16,14 @@ for (i = 0; i <= 256; i++) {
 }
 
 
-const cell = document.querySelectorAll("cell");
+let cell = document.querySelectorAll("cell");
+let cellArray = Array.from(cell);
 
 
 cell.forEach(cell => {cell.addEventListener('mouseenter', () => {
     cell.classList.add("color");
 })});
 
-cell.forEach(cell => {cell.addEventListener('mouseleave', () => {
-    
-})});
 
 
 button.textContent = "Resize grid";
@@ -36,14 +31,29 @@ button.textContent = "Resize grid";
 button.addEventListener('click', () => {
     let response = prompt('How many squares per side would you like the grid to contain?');
     let size = parseInt(response);
+    removeSquares(i);
     createSquares(size);
 
 });
 
+function removeSquares() {
+    for (let i = 0; len = cellArray.length, i < len; i++) {
+        cell[i].remove();
+    }
+    }
+
+
 function createSquares(size) {
-    for (i = 0; i <= size * size; i++) {
-        let cell = document.createElement("cell");
-        cell.id = i;
-        grid.appendChild(cell);
+    for (i = 0; i <= (size * size); i++) {
+        let newCell = document.createElement("newCell");
+        newCell.id = i;
+        grid.appendChild(newCell);
     }
     }
+
+    let newCell = document.querySelectorAll("newCell");
+
+    newCell.forEach(newCell => {newCell.addEventListener('mouseenter', () => {
+        newCell.classList.add("color");
+    })});
+    
