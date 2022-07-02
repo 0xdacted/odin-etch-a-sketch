aaa354ed9d5188fc29f1d4dbbffd4bf0af4c8a69
Red <crabmarket56@gmail.com>
2022-07-02 09:57:52 -0400

    Add button and draft functionality

--- external-js.js
+++ external-js.js
@@ -3,22 +3,47 @@ const grid = document.createElement("grid");
 container.appendChild(grid);
 
 
+const content = document.querySelector(".content");
+const button = document.createElement("button");
+content.appendChild(button);
+
+let width = container.clientWidth;
+console.log(width);
+
+
+
 for (i = 0; i <= 256; i++) {
     let cell = document.createElement("cell");
     cell.id = i;
     grid.appendChild(cell);
 }
 
+
 const cell = document.querySelectorAll("cell");
-const button = document.querySelector("resize")
+
 
 cell.forEach(cell => {cell.addEventListener('mouseenter', () => {
-    console.log(cell.id);
     cell.classList.add("color");
 })});
 
 cell.forEach(cell => {cell.addEventListener('mouseleave', () => {
-    console.log(cell.id);
+    
 })});
 
 
+button.textContent = "Resize grid";
+
+button.addEventListener('click', () => {
+    let response = prompt('How many squares per side would you like the grid to contain?');
+    let size = parseInt(response);
+    createSquares(size);
+
+});
+
+function createSquares(size) {
+    for (i = 0; i <= size * size; i++) {
+        let cell = document.createElement("cell");
+        cell.id = i;
+        grid.appendChild(cell);
+    }
+    }
